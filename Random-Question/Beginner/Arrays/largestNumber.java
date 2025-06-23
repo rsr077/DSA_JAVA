a40a15ffb78684174c8399abe509eb372789b401
rsr077 <yourfriend77777777@gmail.com>
2025-06-23 19:50:57 +0530

    largestNumber.java

---
+++ Random-Question/Beginner/Arrays/largestNumber.java
@@ -0,0 +1,24 @@
+
+public class largestNumber {
+  public static void main(String[] args) {
+
+int a = 10; 
+int b = 20 ;
+int c = 30;
+
+int larget;
+
+if(a >= b && a >= c)  {
+  larget = a;
+
+}else  if (b >= a && b >= c) {
+  larget = b;
+}else {
+   larget = c;
+}
+
+  
+ 
+  System.out.println(larget);
+  }
+  }
