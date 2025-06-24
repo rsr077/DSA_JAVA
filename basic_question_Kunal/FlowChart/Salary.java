226e68de53afc33c11d3bcffb7c3d574e87c20c3
rsr077 <yourfriend77777777@gmail.com>
2025-06-24 19:50:29 +0530

    Flow_Chart

---
+++ basic_question_Kunal/FlowChart/Salary.java
@@ -0,0 +1,15 @@
+public class Salary {
+
+  public static void main(String[] args) {
+
+    int Salary = 10000;
+
+  if(Salary > 100000) {
+     Salary = Salary + 2000;
+  } else {
+    Salary = Salary + 1000;
+  }
+
+  System.out.println(Salary);
+  }
+}
