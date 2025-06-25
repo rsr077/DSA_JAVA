db1ef52e78ae5af81d3ded9a2510e0a783459680
rsr077 <yourfriend77777777@gmail.com>
2025-06-25 18:46:32 +0530

    Init

---
+++ basic_question_Kunal/First_java_Program/Scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+
+ public class Main {
+   public static void main(String[] args ){
+
+   Scanner input = new Scanner(System.in);
+   
+    int a = input.nextInt();
+    int b = input.nextInt();
+
+   int ans =  a + b;
+
+   System.out.println(ans);
+ }
+ }
