fae8c9861eb3fd88f67bf0ac45a2c8bb4329d20f
rsr077 <yourfriend77777777@gmail.com>
2025-06-26 18:22:37 +0530

    Init

---
+++ basic_question_Kunal/conditiona_loops/IfSalary.java
@@ -0,0 +1,13 @@
+
+public class IfSalary {
+  public static void main(String[] args) {
+
+    int Salary = 1000;
+    if(Salary > 10000) {
+       Salary = Salary + 1000;
+    }else {
+      Salary = Salary + 2000;
+    }
+         System.out.println(Salary);
+  }
+}
