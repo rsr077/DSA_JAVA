356299f0a2e2f0fef51aef1872aad3e582bc4c5b
rsr077 <yourfriend77777777@gmail.com>
2025-06-29 15:04:01 +0530

    Init

--- Mid/Function_Method/Sum.java
+++ Mid/Function_Method/Sum.java
@@ -1,4 +1,4 @@
-
+/* 
 import java.util.Scanner;
 
 public class Sum {
@@ -22,6 +22,37 @@ public class Sum {
    int  sum = num1 + num2;
 
    System.out.println("Your are answer is"  + " " + sum);
+
+  }
+
+ }
+
+ */
+
+
+//  return the  value 
+
+
+
+import java.util.Scanner;
+
+public class Sum {
+  public  static void main (String[]  args) {
+
+      int ans = sum2();
+      System.out.println(ans); 
+
+}
+
+ static void sum2() {
+   Scanner in = new Scanner(System.in);
+
+   System.out.print(" Emter numberOne");
+   int  num1 = in.nextInt();
+   System.out.print(" Enter numberTwo");
+   int num2  = in.nextInt();
+   int  sum = num1 + num2;
+    return sum;
 
   }
 
---
+++ Mid/Function_Method/Swap.java
@@ -0,0 +1,9 @@
+
+public class Swap {
+   public static void main(String[] args) {
+  
+    
+        System.out.println(a+ " " + b);
+   }
+
+}
