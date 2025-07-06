1e1c815f9ad02bbd921d6b736f5f0b499a1bf23e
rsr077 <yourfriend77777777@gmail.com>
2025-07-06 07:07:04 +0530

    recursion

---
+++ LinearSearch/SearchInStrings.java
@@ -0,0 +1,24 @@
+package LinearSearch;
+
+public class SearchInStrings {
+
+  public static void main(String[] args) {
+    String name = "Rajeev";
+    char target = 's';
+    System.out.println(search(name, target));
+  }
+
+  static boolean search(String str, char target) {
+     if(str. length() == 0) {
+       return false;
+     }
+
+     for(int i = 0; i< str.length(); i++)  {
+       if(target == str.charAt(i)) {
+        return true;
+       }
+     }
+     return false;
+  }
+  
+}
