c317513a18809cffc8724af3c10e860f4e6f391a
rsr077 <yourfriend77777777@gmail.com>
2025-07-13 09:13:13 +0530

    Init

---
+++ Solve.java
@@ -0,0 +1,14 @@
+public class Solve {
+   public static void main(String[] args) {
+        int a = 10;
+        int b = 20 ;
+        int c = 30;
+
+          int ans = a;
+        if(a > ans) {
+        ans = b;
+        }else if (b > ans) {
+           ans = c;
+        }
+   }
+}
