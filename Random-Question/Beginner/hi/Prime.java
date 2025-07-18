016bfc8559f0d4b436305e463a31d5846cecfaa3
rsr077 <yourfriend77777777@gmail.com>
2025-07-18 07:19:13 +0530

    Init

--- Random-Question/Beginner/hi/Prime.java
+++ Random-Question/Beginner/hi/Prime.java
@@ -1,43 +1,30 @@
+package com.kunal;
+
 import java.util.Scanner;
 
-
 public class Prime {
-
     public static void main(String[] args) {
-        int num = 2;
-
-        for(int i = 2; i< num; i++){
-            if(num % i == 0){
-                System.out.println(num + " is NOT a               Prime Number.");
-                return;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int n = in.nextInt();
+        if(n <= 1) {
+            System.out.println("Neither prime nor composite");
+            return;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                System.out.println("Not Prime");
+                c++;
+            }else {
+                System.out.println("Prime");
             }
+          /*   c = c + 1; */
+            // c++;..
         }
+        /* if (c * c > n) {
+            System.out.println("Prime");
+        } */
+        
     }
 }
-
-
-/* public class Prime { 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-
-        if (isPrime(num)) {
-            System.out.println(num + " is a Prime Number.");
-        } else {
-            System.out.println(num + " is NOT a Prime Number.");
-        }
-    }
-
-    public static boolean isPrime(int number) {
-        if (number <= 1) return false;
-        if (number == 2) return true;
-        if (number % 2 == 0) return false;
-
-        for (int i = 3; i <= Math.sqrt(number); i += 2) {
-            if (number % i == 0) return false;
-        }
-        return true;
-    }
-}
-    */ 
--- Solve.java
+++ Solve.java
@@ -1,14 +1,13 @@
+import java.util.Scanner;
+
 public class Solve {
    public static void main(String[] args) {
-        int a = 10;
-        int b = 20 ;
-        int c = 30;
+         char ch = 'a';
 
-          int ans = a;
-        if(a > ans) {
-        ans = b;
-        }else if (b > ans) {
-           ans = c;
-        }
+         if(ch > 'A'  && ch < 'z' ) {
+             System.out.println("L");
+         } else {
+            System.out.println("U");
+         }
    }
 }
